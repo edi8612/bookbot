a8ede804eeb2cd14864a8d747327f4838bc63937
edi8612 <edi.sulo01@gmail.com>
2026-04-02 02:22:51 +0200

    added a function to sort the list of dictionary and removed hardcoded book paths

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from stats import count_book_words
+import sys
+from stats import count_book_words, count_book_char, sorted_list_dict
+
 
 def main():
-    num_words = count_book_words("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    book_path = sys.argv[1]
+
+    num_words = count_book_words(book_path)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    char_count = count_book_char(book_path)
+
+    sorted_char_list = sorted_list_dict(char_count)
+    
+    for char_dict in sorted_char_list:
+        print(f"{char_dict['char']}: {char_dict['num']}")
+
+    print("============= END ===============")
+
+    
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,34 @@ def count_book_words(text):
     words = text_book.split()
     count_words = len(words)
     return count_words
+
+def count_book_char(text):
+    text_book = get_book_text(text)
+    text_book_lower = text_book.lower()
+
+    counted = {}
+    for char in text_book_lower:
+        if char not in counted:
+            counted[char] = 1
+        else:
+            counted[char] += 1
+    
+    return counted
+
+def sorted_dict_helper(text):
+    return text["num"]
+
+def sorted_list_dict(char_count):
+    char_list = []
+    for char, count in char_count.items():
+        if char.isalpha():
+            char_list.append({"char": char, "num": count})
+    
+    char_list.sort(key=sorted_dict_helper, reverse=True)
+    return char_list
+
+
+
+    
+
+
